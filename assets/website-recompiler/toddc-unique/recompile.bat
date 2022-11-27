62d1302489dc913791b2cd5410efcc64e44f873b
FistingUranus <77898683+Fis-Ura@users.noreply.github.com>
2022-11-27 15:49:55 -0500

    unique

---
+++ assets/website-recompiler/toddc-unique/recompile.bat
@@ -0,0 +1,4 @@
+curl -L "https://docs.google.com/spreadsheets/d/1CphbUBulbyEK_Mm_fG0suXDLwo9xHWF2p1jhLmDHn3Y/gviz/tq?tqx=out:csv&sheet=Unique&range=A:B" > "..\..\destiny-dc\items\unique\csv\unique.csv"
+
+python main.py
+pause
